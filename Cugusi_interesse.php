c8ab09b0902900dee650aedf0ab90cc6f0db5e8a
Mattia Cugusi <m.cugusi287gmail.com>
2024-09-26 15:59:12 +0200

    inizio esercizio

---
+++ Cugusi_interesse.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Interesse</title>
+</head>
+<body>
+    <?php
+    $capitale = 25000;
+    $tasso = 7;
+    $numGiorni = 30;
+
+    echo "<p>Capitale = " . $capitale . " €";
+    echo 
+
+    ?>
+    
+</body>
+</html>
