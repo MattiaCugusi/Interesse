27de339aadba9aa0dffb69e04fb53f5741d6845d
Mattia Cugusi <m.cugusi287gmail.com>
2024-09-26 16:09:16 +0200

    esercizio completato

--- Cugusi_interesse.php
+++ Cugusi_interesse.php
@@ -10,9 +10,19 @@
     $capitale = 25000;
     $tasso = 7;
     $numGiorni = 30;
+    $interesse;
 
-    echo "<p>Capitale = " . $capitale . " €";
-    echo 
+
+
+    echo "<p> Capitale = " . $capitale . " €";
+    echo "<p> Tasso = " . $tasso . " €";
+    echo "<p> Num Giorni = " . $numGiorni;
+
+    $interesse = ($capitale * $tasso * $numGiorni)/36500;
+
+    $interesse = number_format($interesse, 2);
+
+    echo "<h2 style= 'color: green;'>L'interesse calcolato e' di " . $interesse . " € ";
 
     ?>
     
